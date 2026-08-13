54f01a880a3af7ca881414430687fb839dcf65aa
leonardfeifel <leonardfeifel@gmail.com>
2026-08-13 13:38:51 +0200

    adds course Feature display name mapping

--- client/src/types/CourseFeature.ts
+++ client/src/types/CourseFeature.ts
@@ -4,3 +4,10 @@ export enum CourseFeature {
     HAPPINESS_INDEX = "HAPPINESS_INDEX",
     CODE_ACTIVITY = "CODE_ACTIVITY",
 }
+
+export const CourseFeatureDisplayNames: Record<CourseFeature, string> = {
+    [CourseFeature.COMPETITION]: "Competition",
+    [CourseFeature.STANDUPS]: "Standups",
+    [CourseFeature.HAPPINESS_INDEX]: "Happiness Index",
+    [CourseFeature.CODE_ACTIVITY]: "Code Activity",
+};
