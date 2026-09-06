fe20d3d8ddc8a870fca44f5bdae389361cb9ce9c
leonardfeifel <leonardfeifel@gmail.com>
2026-09-06 14:34:24 +0200

    fix failed tests by adding standup feature to test course

--- server/src/tests/api/helpers/testDb.ts
+++ server/src/tests/api/helpers/testDb.ts
@@ -52,8 +52,8 @@ export async function seedDatabase(db: Database) {
 
   // Create test course
   await db.run(
-    `INSERT INTO courses (courseName, termId, studentsCanCreateProject, enabledFeatures) VALUES (?, ?, ?, ?)`,
-    ['Test Course', 1, true, JSON.stringify([CourseFeature.HAPPINESS_INDEX])]
+    `INSERT INTO courses (id, courseName, termId, studentsCanCreateProject, enabledFeatures) VALUES (?, ?, ?, ?, ?)`,
+    [1, 'Test Course', 1, true, JSON.stringify([CourseFeature.HAPPINESS_INDEX, CourseFeature.STANDUPS])]
   );
 
   // Create test project
